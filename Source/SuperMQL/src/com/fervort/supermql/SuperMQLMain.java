5f12634e4333ec116c4e153d4559409de4d5c313
fervorT <fervort.com@gmail.com>
2019-10-19 18:01:50 +0200

    support to interactive query execution, fill execution option

--- Source/SuperMQL/src/com/fervort/supermql/SuperMQLMain.java
+++ Source/SuperMQL/src/com/fervort/supermql/SuperMQLMain.java
@@ -1,9 +1,14 @@
 package com.fervort.supermql;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 import java.util.Scanner;
 
 import matrix.db.Context;
@@ -32,25 +37,25 @@ public class SuperMQLMain {
 			SuperMQLSupport gss = new SuperMQLSupport();
 			gss.setCurrentContext(context);
 			gss.buildMQLCommand();
-			// Show tip of the day before starting
+			
 			Scanner scanner = new Scanner(System.in);
-			System.out.println("Welcome to SuperMQL");
-			System.out.println("");
-			System.out.println("To execute mql start with dot, For ex=> .temp query bus or .print bus");
-			System.out.println("");
-			System.out.println("SuperMQL, Version 0.0.0.alpha");
-			System.out.println("");
+			
+			printWelcomeMessage();
+			
 			int i=1;
 			
 			String strUserInput;
 			do
 			{
 				System.out.print("Smql<"+i+">");
-				strUserInput = scanner.nextLine();
+				strUserInput = scanner.nextLine().trim();
 				
-				if(strUserInput.trim().startsWith("."))
+				if(strUserInput.length()==0)// if enter multiple time
+					continue;
+				
+				if(strUserInput.startsWith("."))
 				{
-					String strAsMQL = strUserInput.trim();
+					String strAsMQL = strUserInput;
 					try
 					{
 						String strMQL = strAsMQL.substring(1, strAsMQL.length()).trim();
@@ -60,18 +65,89 @@ public class SuperMQLMain {
 					{
 						System.out.println(ex);
 					}
-				}else //it is script
+				//it is script	
+				}
+				else if(strUserInput.startsWith("f ")) 
 				{
-					new GroovyScriptBuilder().buildGroovyScript(context,strUserInput);
+					try
+					{
+						String filePath = strUserInput.substring(1, strUserInput.length()).trim();
+						// TODO check file is valid ?
+						String fileContent = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8); 
+						new GroovyScriptBuilder().buildGroovyScript(context,fileContent);
+					}catch(Exception ex)
+					{
+						System.out.println(ex);	
+					}
+				}
+				//else if(!strUserInput.equalsIgnoreCase("quit") && !strUserInput.equalsIgnoreCase("exit"))
+				else if(strUserInput.startsWith("i "))
+				{
+					try
+					{
+					// TODO add try catch block otherwise if something will fail, control will go out of loop
+					new GroovyScriptBuilder().buildGroovyScript(context,strUserInput.substring(1, strUserInput.length()).trim());
+					System.out.println("");
+					
+					}catch(Exception ex)
+					{
+						System.out.println(ex);	
+					}
+				}else if(strUserInput.startsWith("myq "))
+				{
+					storeMyQuery(strUserInput);
 				}
 				
 				
 				
 				i++;
-			}while(!strUserInput.trim().equalsIgnoreCase("quit") && !strUserInput.trim().equalsIgnoreCase("exit"));
+			}while(!strUserInput.equalsIgnoreCase("quit") && !strUserInput.equalsIgnoreCase("exit"));
 			System.out.println("Bye Bye :)");
 			
 		}
+	}
+	List myQueries = new ArrayList<String>();
+	private void storeMyQuery(String myQuery)
+	{
+		if(myQuery.contains("=>"))
+		{
+			String[] aMyQueries = myQuery.split("=>");
+			
+			try (OutputStream output = new FileOutputStream("SuperMQL.properties")) {
+
+	            Properties prop = new Properties();
+
+	            // set the properties value
+	            prop.setProperty(aMyQueries[0], aMyQueries[1]);
+	         
+	            prop.store(output, null);
+
+	            System.out.println(prop);
+
+	        } catch (IOException io) {
+	            io.printStackTrace();
+	        }
+			
+		}else
+		{
+			System.out.println("Wrong Syntax. Correct syntax is:");
+		}
+		
+	}
+	private void printWelcomeMessage()
+	{
+		// TODO Show tip of the day before starting
+		System.out.println("Welcome to SuperMQL");
+		System.out.println("");
+		System.out.println("To execute native mql start with dot, For ex=> Smql<1>.temp query bus or Smql<3>.print bus");
+		System.out.println("");
+		System.out.println("To execute java/groovy script from file: Smql<1>f D:\\Project\\script\\RenameBus.groovy");
+		System.out.println("");
+		System.out.println("To execute java/groovy script in Interactive mode: Smql<1>i def x = 100; println x;");
+		System.out.println("MQL funcion: Smql<1>i def result=mql('list vault *'); print result");
+		System.out.println("");
+		System.out.println("SuperMQL, Version 0.0.0.alpha");
+		System.out.println("");
 	}
 
 }
